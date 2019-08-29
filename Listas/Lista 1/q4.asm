108ef931a7f97e155bb4396fa90afe7b19db59f8
Guilherme Afonso <guilherme.asp@live.com>
2019-08-29 20:08:05 -0300

    Questão 4 pronta

--- Listas/Lista 1/q4.asm
+++ Listas/Lista 1/q4.asm
@@ -1,7 +1,8 @@
 .data
 
-A: .word 1
-B: .word 1
+L1: .word 6
+L2: .word 3
+L3: .word 2
 R: .word 0
 
 
@@ -9,9 +10,12 @@ R: .word 0
 lw $t1, L1 #Lado 1 esta em $t1
 lw $t2, L2 #Lado 2 esta em $t2
 lw $t3, L3 #Lado 3 esta em $t3
+lw $t7, R #Resposta está em t7
 
-####DefiniÃ§Ã£o de VariÃ¡veis
-addi $s0, $zero, 1 ## $s0 Ã© 1
+####Definição de Variáveis
+addi $s0, $zero, 1 ## $s0 é 1
+addi $s1, $s0, 1 ##s1 é 2
+addi $s2, $s1, 1 ##s2 é 3
 
 #Verificando qual lado é o maior
 sgt $t4, $t2, $t1
@@ -31,17 +35,36 @@ L1MAIOR:
 	bgt $t4, $t1, OK
 	j ZERO
 L2MAIOR:
-	add $t4, $t2, $t3
-	bgt $t4, $t1, OK
+	add $t4, $t1, $t3
+	bgt $t4, $t2, OK
 	j ZERO
 L3MAIOR:
-	add $t4, $t2, $t3
-	bgt $t4, $t1, OK
+	add $t4, $t1, $t2
+	bgt $t4, $t3, OK
 	j ZERO
 ZERO:
-	sw $t7, $zero
+	sw $zero, R
+	j Endif
 
 OK:
+	#L1 = L2 || L1 = L3 || L2 = L3
+	beq $t1, $t2, BEQ
+	beq $t1, $t3, ISOSCELES
+	beq $t2, $t3, ISOSCELES
 	
-
+	#salvando o triangulo como escaleno
+	sw $s2, R
+	
+	j Endif
+BEQ:
+	#teste para ser equilátero.
+	#se não for, ele é isósceles
+	beq $t2, $t3 EQUILATERO
+ISOSCELES:
+	
+	sw $s1, R
+	j Endif
+	
+EQUILATERO:
+	sw $s0, R
 Endif:
