af99c0a5b83fc2476b56ad98aec31c24afd51b96
tiago-ds <42779343+tiago-ds@users.noreply.github.com>
2019-10-01 20:13:20 -0300

    Pequeno ajuste de correção no comentário

--- Listas/ListaVerilog/questao1.v
+++ Listas/ListaVerilog/questao1.v
@@ -7,25 +7,25 @@ module sinal(clock, botao, verde, amarelo, vermelho);
 	output reg amarelo;
 	output reg vermelho;
 	
-	// Setando contagem, que tem que ter 5 bits pra chegar atÈ 30
+	// Setando contagem, que tem que ter 5 bits pra chegar at√© 30
 	reg[4:0] contagem;
-	// Setando estado, que tem que ter 2 bits, pq s„o 4 estados
+	// Setando estado, que tem que ter 2 bits, pq s√£o 4 estados
 	reg[1:0] estado;
 	
 	// Definindo cada estado
 	
-	// VD È VerDe
+	// VD √© VerDe
 	parameter VD=2'b00;
-	// AM È AMarelo
+	// AM √© AMarelo
 	parameter AM=2'b01;
-	// VM È VerMelho
+	// VM √© VerMelho
 	parameter VM=2'b10;
-	// VB È VerdeBotao
+	// VB √© VerdeBotao
 	parameter VB=2'b11;
 	
-	// … tipo o reset
+	// √â tipo o reset
 	initial begin
-		// Estado inicial t· verde, mas n„o t· verde de verdade(n„o conta atÈ o primeiro clock)
+		// Estado inicial t√° verde, mas n√£o t√° verde de verdade(n√£o conta at√© o primeiro clock)
 		estado = 2'b00;
 		verde = 1'b0;
 		amarelo = 1'b0;
@@ -39,16 +39,16 @@ module sinal(clock, botao, verde, amarelo, vermelho);
 			
 			// Se estiver no verde(se o estado for = 00)
 			VD: begin
-				/* Seta o estado vermelho como 0(notar que n„o precisamos setar o amarelo tambÈm, porque
+				/* Seta o estado vermelho como 0(notar que n√£o precisamos setar o amarelo tamb√©m, porque
 				ele nunca vai do amarelo pro verde) */
 				vermelho = 1'b0;
-				// Como t· no estado verde, a gente bota o flag do verde como 1
+				// Como t√° no estado verde, a gente bota o flag do verde como 1
 				verde = 1'b1;
-				// Se apertar o bot„o nesse estado
+				// Se apertar o bot√£o nesse estado
 				if(botao == 1) begin
-					// A contagem vai multiplicar por 2
+					// Gambiarra desenvolvida pra n√£o dar problema em casos espec√≠ficos(cr√©ditos a ropj)
 					contagem = contagem + ((19 - contagem)/2);
-					// E aÌ vamos pro estado do bot„o verde pressionado
+					// E a√≠ vamos pro estado do bot√£o verde pressionado
 					estado = VB;
 				end
 				else begin
@@ -58,7 +58,7 @@ module sinal(clock, botao, verde, amarelo, vermelho);
 						estado = AM;
 						contagem = 5'b00000;
 					end
-					// Sen„o, sÛ soma um na contagem mesmo.
+					// Sen√£o, s√≥ soma um na contagem mesmo.
 					else begin
 						estado = VD;
 						contagem = contagem + 1;
@@ -66,9 +66,9 @@ module sinal(clock, botao, verde, amarelo, vermelho);
 				end
 			end
 			
-			// Caso estivermos no estado do verde com o bot„o tendo sido pressionado
+			// Caso estivermos no estado do verde com o bot√£o tendo sido pressionado
 			VB: begin
-				// Setando vermelho para 0, caso o bot„o tenha sido apertado no comeÁo do verde
+				// Setando vermelho para 0, caso o bot√£o tenha sido apertado no come√ßo do verde
 				vermelho = 1'b0;
 				// Setando verde para 1 pelo mesmo motivo
 				verde = 1'b1;
@@ -83,10 +83,10 @@ module sinal(clock, botao, verde, amarelo, vermelho);
 				end
 			end
 			
-			// Para o estado amarelo, a ˙nica diferenÁa È que ele muda de estado em 9, n„o em 19
+			// Para o estado amarelo, a √∫nica diferen√ßa √© que ele muda de estado em 9, n√£o em 19
 			AM: begin
 				// Setamos o verde para 0, caso seja a primeira rodada do amarelo...
-				/* (Mais uma vez notar que n„o mexemos no vermelho, pois ele sempre chegar· no amarelo
+				/* (Mais uma vez notar que n√£o mexemos no vermelho, pois ele sempre chegar√° no amarelo
 				a partir do verde. */
 				verde = 1'b0;
 				// E o amarelo pra 1
@@ -98,7 +98,7 @@ module sinal(clock, botao, verde, amarelo, vermelho);
 					contagem = 5'b00000;
 				end
 				else begin
-					// Sen„o, sÛ soma um na contagem e reafirma o estado amarelo.
+					// Sen√£o, s√≥ soma um na contagem e reafirma o estado amarelo.
 					estado = AM;
 					contagem = contagem + 1;
 				end
@@ -116,7 +116,7 @@ module sinal(clock, botao, verde, amarelo, vermelho);
 					estado = VD;
 					contagem = 5'b00000;
 				end
-				// Sen„o, apenas reafirmamos o estado vermelho, e somamos 1 na contagem.
+				// Sen√£o, apenas reafirmamos o estado vermelho, e somamos 1 na contagem.
 				else begin
 					estado = VM;
 					contagem = contagem + 1;
